d2a05ebb597b027909056f33b386e509921e28d8
Amitesh218 <amiteshrawal1@gmail.com>
2025-01-23 15:25:50 +0530

    added apicall test for gemini 2.0 flash

---
+++ firstreq.py
@@ -0,0 +1,7 @@
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyBKDjmVVln_MYuo7Nt2Of95zH-HRFOFFNA")
+model = genai.GenerativeModel("gemini-2.0-flash-exp")
+response = model.generate_content("Explain how AI works")
+print(response.text)
+
